5db3fd276fd45f33a25b3805fb579ec23be07e2b
micoos <micoos@163.com>
2018-03-16 17:13:52 +0800

    提交
    不明

---
+++ MiCoos.Blog/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiCoos.Blog.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult Login(UserLoginInfo info)
+		{
+
+			return Json(new { success=true,error=""});
+		}
+	}
+
+	public class UserLoginInfo
+	{
+		public string UserName { get; set; }
+
+		public string Password { get; set; }
+	}
+}
