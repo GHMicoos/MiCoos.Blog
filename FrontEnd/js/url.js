6650e9b59a499c51428b2c4b61b655adb9c03c48
MiCoos <micoos@163.com>
2018-10-24 21:23:02 +0800

    jj

---
+++ FrontEnd/js/url.js
@@ -0,0 +1,9 @@
+var getUrl=function(param){
+    if(param==="bootstrap.min.css")return "../components/bootstrap-3.3.7-dist/css/bootstrap.min.css";
+
+},
+
+var Url={
+    "bootstrap_min_css":"../components/bootstrap-3.3.7-dist/css/bootstrap.min.css"
+
+}
